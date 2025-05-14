04ca548040ebef8fa69b196f4d3517c62dd2bf96
n1ghtmare-dev <oboev3691@gmail.com>
2025-05-14 14:57:17 +0300

    last improve callbacks for merge

--- src/college_bot/handlers/callbacks/main.py
+++ src/college_bot/handlers/callbacks/main.py
@@ -16,10 +16,7 @@ async def add_headmen(callback: CallbackQuery):
     user_id = callback.from_user.id
     user_group = get_user_group(user_id)[0]['group_id']
 
-    print("GROUP:", user_group)
     group_headmen = json_manager.get_headmen(user_group)
-
-    print(group_headmen)
 
     if len(group_headmen) < 2:
         if user_id not in group_headmen:
