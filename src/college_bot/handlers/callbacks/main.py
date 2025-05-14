cb6de80ba8caa1d7c4b37a66b7b8156b793f4f0d
n1ghtmare-dev <oboev3691@gmail.com>
2025-05-14 14:18:12 +0300

    Improve callback function add_headmen

--- src/college_bot/handlers/callbacks/main.py
+++ src/college_bot/handlers/callbacks/main.py
@@ -14,17 +14,20 @@ json_manager = GroupsUpdater(settings.DB_CONFIG)
 @router.callback_query(F.data == 'become_leader')
 async def add_headmen(callback: CallbackQuery):
     user_id = callback.from_user.id
-    user_group = get_user_group(user_id)
+    user_group = get_user_group(user_id)[0]['group_id']
+
+    print("GROUP:", user_group)
     group_headmen = json_manager.get_headmen(user_group)
 
+    print(group_headmen)
+
     if len(group_headmen) < 2:
-        ...
+        if user_id not in group_headmen:
+            if json_manager.add_headman(user_id, user_group):
+                await callback.answer(f"Вы добавлены как староста группы {user_group}")
+        else:
+            await callback.answer("Вы уже в списке старост этой группы")
     else:
-        pass
-    
-    
-    # get group by user_id
-    # check headmen in group
-    # add user in group's headmen
+        await callback.answer("В вашей группе уже 2 старосты")
 
     
